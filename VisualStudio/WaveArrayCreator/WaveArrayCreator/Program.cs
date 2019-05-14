bb63ff3cfdcf9714381f8f2bedfc0508cd7072b2
Mhage <masahage@soleil.ocn.ne.jp>
2019-05-14 20:50:36 +0900

    Add WaveArrayCreator
    WaveArrayCreator creates a code showing data of wave file.

---
+++ VisualStudio/WaveArrayCreator/WaveArrayCreator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace WaveArrayCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data;
+            using (var fs = new FileStream("tesris8.wav", FileMode.Open))
+            {
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+            }
+            using (var writer = new StreamWriter("tetris_wav.h"))
+            {
+                writer.Write("const byte tetris_wav[] = { " + Environment.NewLine);
+                for (int i = 0; i < data.Length; ++i)
+                {
+                    writer.Write("0x" + data[i].ToString("X2")
+                        + (i != (data.Length - 1) ? ", " : "")
+                        + (i % 16 == 15 ? Environment.NewLine : ""));
+                }
+                writer.Write(" };");
+            }
+        }
+    }
+}
